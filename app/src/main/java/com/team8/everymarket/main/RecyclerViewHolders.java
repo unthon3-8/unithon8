c192764416158246fa2c268cae15bce04b6a0e31
chaeeun <chaeeun218@gmail.com>
2017-02-05 02:33:26 +0900

    MainActivity recycelrviewAdapter, ViewHolder작성

---
+++ app/src/main/java/com/team8/everymarket/main/RecyclerViewHolders.java
@@ -0,0 +1,28 @@
+package com.team8.everymarket.main;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.team8.everymarket.R;
+
+
+public class RecyclerViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+    public TextView farmName;
+    public ImageView farmPhoto;
+
+    public RecyclerViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+        farmName = (TextView)itemView.findViewById(R.id.country_name);
+        farmPhoto = (ImageView)itemView.findViewById(R.id.country_photo);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(view.getContext(), "Clicked Country Position = " + getPosition(), Toast.LENGTH_SHORT).show();
+    }
+}
